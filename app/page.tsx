596a4b4a0e41ac64a55855a221ae5f20d238ceae
trevsuth <trevor.sutherland@gmail.com>
2023-12-25 20:04:35 -0500

    started cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import Sidebar from '../components/Sidebar';
+import styles from './Home.module.css'; // Import only if using CSS Modules
 
 export default function Home() {
   return (
     <>
       <Sidebar />
-      <div>Home Page</div>
+      <div className={styles.homeContainer}> {/* Update this line */}
+        Home Page
+      </div>
     </>
   );
 }
