e1e5fda800573b6a25fd1317e00185ba233890db
trevsuth <trevor.sutherland@gmail.com>
2023-12-25 19:26:35 -0500

    initial commit

---
+++ app/interface/page.js
@@ -0,0 +1,10 @@
+import Sidebar from '../../components/Sidebar'
+
+export default function Interface() {
+    return (
+        <>
+            <Sidebar />
+            <div>Interface Page</div>
+        </>
+    );
+  }  
---
+++ components/Sidebar.js
@@ -0,0 +1,4 @@
+export default function Sidebar() {
+    return <div style={{ width: '25%', float: 'left', background: '#eee' }}>Sidebar</div>;
+  }
+  
